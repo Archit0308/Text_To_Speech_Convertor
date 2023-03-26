e64c1ca7eab98a754583433a227a47c70bded7c0
ARCHIT <127728488+Archit0308@users.noreply.github.com>
2023-03-26 22:59:04 +0530

    Update TTS.js

--- TTS.js
+++ TTS.js
@@ -1,72 +1,61 @@
-const fromText = document.querySelector(".from-text");
-const toText = document.querySelector(".to-text");
-const exchangeIcon = document.querySelector(".exchange");
-const selectTags = document.querySelectorAll("select");
-const icons = document.querySelectorAll(".row i");
-const translateBtn = document.querySelector("button");
+// Define variables and select DOM elements
+const textarea = document.querySelector("textarea");
+const voiceList = document.querySelector("select");
+const speechBtn = document.querySelector("button");
 
-selectTags.forEach((tag, id) => {
-  for (const countryCode in countries) {
-    const selected =
-      id === 0
-        ? countryCode === "en-GB"
-          ? "selected"
-          : ""
-        : countryCode === "hi-IN"
-        ? "selected"
-        : "";
-    const option = `<option ${selected} value="${countryCode}">${countries[countryCode]}</option>`;
-    tag.insertAdjacentHTML("beforeend", option);
+// Initialize speech synthesis and set initial speaking state
+let synth = window.speechSynthesis;
+let isSpeaking = false;
+
+// Call function to populate voice list with available voices
+populateVoiceList();
+function populateVoiceList() {
+  const voices = synth.getVoices();
+  for (const voice of voices) {
+    const option = document.createElement("option");
+    option.textContent = `${voice.name} (${voice.lang})`;
+    option.value = voice.name;
+    voiceList.appendChild(option);
   }
-});
+}
 
-exchangeIcon.addEventListener("click", () => {
-  let tempText = fromText.value;
-  let tempLang = selectTags[0].value;
-  fromText.value = toText.value;
-  toText.value = tempText;
-  selectTags[0].value = selectTags[1].value;
-  selectTags[1].value = tempLang;
-});
+// Update voice list when voices change
+synth.addEventListener("voiceschanged", populateVoiceList);
 
-fromText.addEventListener("input", () => {
-  if (!fromText.value.trim()) {
-    toText.value = "";
-  }
-});
-
-translateBtn.addEventListener("click", () => {
-  const text = fromText.value.trim();
-  const translateFrom = selectTags[0].value;
-  const translateTo = selectTags[1].value;
-  if (!text) return;
-  toText.setAttribute("placeholder", "Translating...");
-  const apiUrl = `https://api.mymemory.translated.net/get?q=${text}&langpair=${translateFrom}|${translateTo}`;
-  fetch(apiUrl)
-    .then(res => res.json())
-    .then(data => {
-      const translation = data.responseData.translatedText;
-      toText.value = translation;
-      data.matches.forEach(match => {
-        if (match.id === 0) {
-          toText.value = match.translation;
-        }
-      });
-      toText.setAttribute("placeholder", "Translation");
-    });
-});
-
-icons.forEach(icon => {
-  icon.addEventListener("click", ({ target }) => {
-    const text = target.id === "from" ? fromText.value : toText.value;
-    const lang = target.id === "from" ? selectTags[0].value : selectTags[1].value;
-    if (!text.trim()) return;
-    if (target.classList.contains("fa-copy")) {
-      navigator.clipboard.writeText(text);
-    } else {
-      const utterance = new SpeechSynthesisUtterance(text);
-      utterance.lang = lang;
-      speechSynthesis.speak(utterance);
+// Function to convert text to speech
+function textToSpeech(text) {
+  const utterance = new SpeechSynthesisUtterance(text);
+  const selectedVoice = voiceList.value;
+  const voices = synth.getVoices();
+  for (const voice of voices) {
+    if (voice.name === selectedVoice) {
+      utterance.voice = voice;
     }
-  });
+  }
+  synth.speak(utterance);
+}
+
+// Event listener for speech button click
+speechBtn.addEventListener("click", (e) => {
+  e.preventDefault();
+  const text = textarea.value.trim();
+  if (text) {
+    if (synth.speaking) {
+      // If speaking, pause or resume speech depending on current state
+      if (synth.paused) {
+        synth.resume();
+        isSpeaking = false;
+        speechBtn.innerText = "Pause Speech";
+      } else {
+        synth.pause();
+        isSpeaking = true;
+        speechBtn.innerText = "Resume Speech";
+      }
+    } else {
+      // If not speaking, convert text to speech
+      textToSpeech(text);
+      isSpeaking = true;
+      speechBtn.innerText = "Pause Speech";
+    }
+  }
 });
